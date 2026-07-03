a4586e21ff3fb14e041402f0b45959734dea90d1
Dileep yadav <dy695354612@gmail.com>
2026-07-03 13:01:24 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,117 @@
+// function hello(){
+//     console.log("hello");
+// }
+
+
+
+// function printName(){
+//     console.log("Apna college");
+//     console.log("Apna college");
+// }
+
+// function print1to5() {
+//     for(let i=1; i<=5; i++){
+//         console.log(i);
+//     }
+// }
+
+
+// function isAdult(){
+//     let age = 17 ;
+//     if(age >=18){
+//         console.log("Adult");  
+//     }
+//     else {
+//         console.log("not adult"); 
+//     }
+// }
+
+// isAdult();
+
+
+// function printPoem(){
+//     console.log("Twinkle Twinkle little star");
+// }
+
+// printPoem();
+
+// function rolldice(){
+//     let random = Math.floor(Math.random()*6)+1;
+//     console.log(random);
+// }
+
+// rolldice();
+
+// function printinfo(name, age){
+//     console.log(`${name}'s age is ${age}.`);
+// }
+
+// printinfo("Dileep",18);
+// printinfo("dileep");
+
+// function sum(a,b){
+//     console.log(a+b);
+// }
+
+// sum(2,4);
+// sum(4,5);
+
+
+// function calcAvg(a,b,c){
+//     avg = (a+b+c)/2;
+//     console.log(avg);
+// }
+
+// calcAvg(2,3,4);
+
+// function printTable(a){
+//     for(let i=a; i<=a*10; i=i+a){
+//         console.log(i);
+//     }
+// }
+
+// printTable(2);
+
+// function isAdult(age){
+//     if(age >= 18){
+//         return "adult";
+//     }
+//     else {
+//         return "not adult";
+//     }
+// }
+
+// console.log(isAdult(13));
+
+// function sumNum(n){
+//     let sum = 0;
+//     for(let i=1; i<=n; i++){
+//         sum = sum + i;
+//     }
+//     return sum; 
+// }
+
+// console.log(sumNum(100));
+
+
+// let str = ["hi", "hello", "bye", "!"];
+
+// function concat(str) {
+//     let result = "  ";
+
+//     for(let i=0; i<str.length; i++){
+//         result += str[i];
+//     }
+
+//     return result;  
+// }
+
+
+function outerfunc(){
+    let x = 5;
+    let y = 6;
+    function innerFunc() {
+        console.log(x);
+    }
+    innerFunc();
+}
