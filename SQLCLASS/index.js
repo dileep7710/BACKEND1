2bdb2b4d8b771338ead2112844e0f982acf0a644
Dileep Yadav <dy695354612@gmail.com>
2026-08-22 23:16:08 +0530

    backend database

---
+++ SQLCLASS/index.js
@@ -0,0 +1,44 @@
+const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+
+const connection =  mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password: '12345'
+});
+
+let getRandomUser = () => {
+  return [
+    faker.string.uuid(),
+    faker.internet.username(),
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+};
+
+// Inserting New Data 
+let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+
+let data = [];
+for(let i=1; i<=100; i++){
+  data.push(getRandomUser()); // 100 fake users
+}
+
+
+try {
+  connection.query(q, [data], (err, result) =>{
+    if(err) throw err;
+    console.log(result);
+   
+  });
+} catch (err) {
+  console.log(err);
+}
+connection.end();
+
+
+
+console.log(getRandomUser());
+
+
